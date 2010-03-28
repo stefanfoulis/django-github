97937679b84d56aef404619d3ba948ffb2bb919a
Charles Leifer <coleifer@gmail.com>
2010-03-27 19:09:19 -0500

    Allow project slug to be edited

--- github/models.py
+++ github/models.py
@@ -72,7 +72,7 @@ class User(models.Model):
 class Project(models.Model):
     user = models.ForeignKey(User, null=True, blank=True)
     title = models.CharField(max_length=255)
-    slug = models.SlugField(blank=True, editable=False)
+    slug = models.SlugField(blank=True)
     description = models.TextField()
     github_repo = models.CharField(max_length=255, blank=True)
     created = models.DateTimeField(auto_now_add=True)
